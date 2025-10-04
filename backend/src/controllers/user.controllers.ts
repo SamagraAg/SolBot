7865b7148acfa089134d1a335edd8a928c5c47bf
Samagra Agrawal <samagra110@gmail.com>
2025-10-04 20:56:33 +0530

    add login and fetch user route

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,3 +1,39 @@
-export const getAllUsers = (req,res)=>{
-    //fetch all users from database
-}
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.model.js";
+import { hash } from "bcrypt";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ success: true, users });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, error: "server error" });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = User.find(email);
+    if (user)
+      return res
+        .status(400)
+        .json({ success: false, error: "Duplicate user exist" });
+    const hashedPassword = await hash(password, 10);
+    const newuser = new User({ name, email, password: hashedPassword });
+    await newuser.save();
+    return res.status(201).json({ success: true, id: newuser._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ success: false, error: "server error" });
+  }
+};

7c730b5ff6200ee4c3f37b10efcdf2313f12a180
Samagra Agrawal <samagra110@gmail.com>
2025-10-05 20:05:54 +0530

    add login route and refactor responses

--- backend/src/controllers/user.controllers.ts
+++ backend/src/controllers/user.controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.model.js";
-import { hash } from "bcrypt";
+import { hash, compare } from "bcrypt";
 
 export const getAllUsers = async (
   req: Request,
@@ -12,7 +12,7 @@ export const getAllUsers = async (
     return res.status(200).json({ success: true, users });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ success: false, error: "server error" });
+    return res.status(500).json({ success: false, message: "server error" });
   }
 };
 
@@ -34,6 +34,35 @@ export const userSignup = async (
     return res.status(201).json({ success: true, id: newuser._id.toString() });
   } catch (error) {
     console.log(error);
-    return res.status(500).json({ success: false, error: "server error" });
+    return res.status(500).json({ success: false, message: "server error" });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    //user login
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(401)
+        .json({ success: false, message: "User not found" });
+    }
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res
+        .status(401)
+        .json({ success: false, message: "Incorrect Password" });
+    }
+    return res.status(200).json({ success: true, id: user._id });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "server side error" });
   }
 };
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { getAllUsers, userSignup } from "../controllers/user.controllers.js";
-import { signupValidator, validate } from "../utils/validators.js";
+import { getAllUsers, userLogin, userSignup } from "../controllers/user.controllers.js";
+import { loginValidator, signupValidator, validate } from "../utils/validators.js";
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
 
 //request goes through validation middleware, on success it goes to route handler for signup
 userRouter.post("/signup", validate(signupValidator), userSignup);
+userRouter.post("/login",validate(loginValidator),userLogin);
 
 export default userRouter;

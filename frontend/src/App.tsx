f17f15c52a8f26b13debd1281d89b99350a0aaa0
Samagra Agrawal <samagra110@gmail.com>
2025-10-07 21:42:40 +0530

    Create Login page UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Chat from "./pages/Chat";
-import NotFound from "./pages/NotFound";
+import Header from "./components/Header.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Chat from "./pages/Chat.jsx";
+import NotFound from "./pages/NotFound.jsx";
 
 function App() {
   return (
--- frontend/src/components/shared/CustomizedInput.tsx
+++ frontend/src/components/shared/CustomizedInput.tsx
@@ -1,25 +1,28 @@
 import TextField from "@mui/material/TextField";
+
 type Props = {
   name: string;
   type: string;
   label: string;
 };
-const CustomizedInput = (props: Props) => {
+
+const CustomizedInput = ({ name, type, label }: Props) => {
   return (
     <TextField
+      fullWidth
       margin="normal"
-      name={props.name}
-      label={props.label}
-      type={props.type}
-      slotProps={{
-        inputLabel: { style: { color: "white" } },
-        input: {
-          style: {
-            width: "400px",
-            borderRadius: 10,
-            fontSize: 20,
-            color: "white",
-          },
+      name={name}
+      label={label}
+      type={type}
+      InputLabelProps={{
+        style: { color: "white" },
+      }}
+      InputProps={{
+        style: {
+          borderRadius: 10,
+          fontSize: 16,
+          color: "white",
+          padding: "12px",
         },
       }}
     />

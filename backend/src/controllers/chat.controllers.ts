52c9aacf51c9b3bbba7be033c1d89eede5e9d0d2
Samagra Agrawal <samagra110@gmail.com>
2025-10-09 23:08:29 +0530

    completing chatGeneration route

--- backend/src/controllers/chat.controllers.ts
+++ backend/src/controllers/chat.controllers.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import ai from "../config/googleai-config.js";
-
+import UserModel from "../models/User.model.js";
 
 export const generateChatCompletion = async (
   req: Request,
@@ -9,13 +9,32 @@ export const generateChatCompletion = async (
 ) => {
   try {
     const { message } = req.body;
-    const response = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: message,
-    });
-    res.status(200).json({ success: true, response });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({ success: false, message: "server error" });
+    const user = await UserModel.findById(res.locals.jwtData.id);
+    if (!user)
+      return res.status(401).json({
+        success: false,
+        message: "User not registered OR Token malfunctioned",
+      });
+    // grab chats of user
+    const chats = user.chats.map(({ role, content }) => ({
+      role,
+      parts: [{ text: content }],
+    }));
+    // chats.push({ role: "user", parts: [{ text: message }] });
+
+    // send all chats with new one to gemini API
+    const chat = ai.chats.create({ model: "gemini-2.5-flash", history: chats });
+
+    // get latest response
+    const response = await chat.sendMessage({ message });
+    user.chats.push({ content: message, role: "user" });
+    user.chats.push({ role: "model", content: response.text });
+    await user.save();
+    return res.status(200).json({ success: true, chats: user.chats });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Server side errro " });
   }
 };
--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
 import { verifyToken } from "../utils/token-manager.js";
 import { generateChatCompletion } from "../controllers/chat.controllers.js";
+import { chatCompletionValidator, validate } from "../utils/validators.js";
+
+//protected apis
 const chatRouter = Router();
 
-chatRouter.post("/new", generateChatCompletion);
+chatRouter.post(
+  "/new",
+  validate(chatCompletionValidator),
+  verifyToken,
+  generateChatCompletion
+);
 export default chatRouter;

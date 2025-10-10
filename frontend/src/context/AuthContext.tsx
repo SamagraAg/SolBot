86719cf022487b73966e44d987b31330aef49d8d
Samagra Agrawal <samagra110@gmail.com>
2025-10-10 23:46:18 +0530

    add logout functionality to frontend

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import { createContext, useContext, useEffect, useState } from "react";
-import { checkAuthStatus, loginUser } from "../helpers/api-communicator";
+import { checkAuthStatus, loginUser, logoutUser } from "../helpers/api-communicator";
 
 type User = {
   name: string;
@@ -26,7 +26,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
       if (data) {
         setUser({ email: data.email, name: data.name });
         setIsLoggedIn(true);
-      } 
+      }
     }
     checkStatus();
   }, []);
@@ -38,7 +38,12 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     }
   };
   const signup = async (name: string, email: string, password: string) => {};
-  const logout = async () => {};
+  const logout = async () => {
+    await logoutUser();
+    setIsLoggedIn(false);
+    setUser(null);
+    window.location.reload();
+  };
 
   const value: UserAuth = {
     user,

005d321d4cce273da88a9cb0553cdafda4d0e4eb
Samagra Agrawal <samagra110@gmail.com>
2025-10-14 18:38:51 +0530

    change firebase config

--- frontend/src/config/firebase.ts
+++ frontend/src/config/firebase.ts
@@ -8,14 +8,14 @@ import { getAnalytics } from "firebase/analytics";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "solbot-5530a.firebaseapp.com",
-  projectId: "solbot-5530a",
-  storageBucket: "solbot-5530a.firebasestorage.app",
-  messagingSenderId: "918063293547",
-  appId: "1:918063293547:web:3c54cb48da7701f5eefd2e",
-  measurementId: "G-GZ28ECW78W",
+  authDomain: "sampark-ai1.firebaseapp.com",
+  projectId: "sampark-ai1",
+  storageBucket: "sampark-ai1.firebasestorage.app",
+  messagingSenderId: "1043876975123",
+  appId: "1:1043876975123:web:13e72269f28cfb67ff25e6",
+  measurementId: "G-PLQB23N158",
 };
 
 // Initialize Firebase
-export const firebase_app = initializeApp(firebaseConfig);
-export const analytics = getAnalytics(firebase_app);
+const firebase_app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebase_app);
